30609c607afd777b3ceb8138d1ff5cc27fd3d29f
rgcomins <100152345+rgcomins@users.noreply.github.com>
2022-02-26 03:16:10 -0500

    Add files via upload

---
+++ P1HW1_CominsRichard.py
@@ -0,0 +1,20 @@
+#This program has the user input two numbers from the keyboard.
+#It then displays the numbers entered and performs the following calculations
+#It adds the numbers, and then it multiplies the numbers 
+
+num1 = int(input('Enter a number. '))
+num2 = int(input('Enter a second number. '))
+num3 = (num1) + (num2)
+num4 = (num1) * (num2)
+print('First number entered.' ,num1)
+print('Second numbered entered.',num2)
+print('The sum of adding the two numbers is',num1 + num2)
+print ('The product of multiplying the two numbers is' ,num1 * num2)
+
+
+
+
+
+
+
+                
