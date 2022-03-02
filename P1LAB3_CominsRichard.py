21da167440595248c59fd55f84ffb7f67c682f2f
rgcomins <100152345+rgcomins@users.noreply.github.com>
2022-03-01 20:57:59 -0500

    Add files via upload

---
+++ P1LAB3_CominsRichard.py
@@ -0,0 +1,17 @@
+#This program prints a formatted No-Parking sign
+
+#02/26/2022
+#CTI-110-P1LAB3-NoParking
+#Richard Comins
+
+#psuedocode
+#This program displays a No Parking message
+#Display the message "NO PARKING"
+#Display the hours this no parking will be in effect "2:00  -  6:00a.m."
+
+
+print('  NO PARKING')
+print('2:00   -   6:00 a.m.')
+
+
+
